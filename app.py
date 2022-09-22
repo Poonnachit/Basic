c74386b47dedfd307b547b4b23e9ead89ea7b7c5
Poonnachit <poonnachitam@gmail.com>
2022-09-22 21:24:08 +0700

    update lesson hello world

--- app.py
+++ app.py
@@ -144,5 +144,70 @@ def lesson1_3():
     return render_template("hel3.html")
 
 
+@app.route('/hel4')
+def lesson1_4():
+    return render_template("hel4.html")
+
+
+@app.route('/hel5')
+def lesson1_5():
+    return render_template("hel5.html")
+
+
+@app.route('/hel6')
+def lesson1_6():
+    return render_template("hel6.html")
+
+
+@app.route('/hel7')
+def lesson1_7():
+    return render_template("hel7.html")
+
+
+@app.route('/hel8')
+def lesson1_8():
+    return render_template("hel8.html")
+
+
+@app.route('/hel9')
+def lesson1_9():
+    return render_template("hel9.html")
+
+
+@app.route('/hel10')
+def lesson1_10():
+    return render_template("hel10.html")
+
+
+@app.route('/hel11')
+def lesson1_11():
+    return render_template("hel11.html")
+
+
+@app.route('/hel12')
+def lesson1_12():
+    return render_template("hel12.html")
+
+
+@app.route('/hel13')
+def lesson1_13():
+    return render_template("hel13.html")
+
+
+@app.route('/hel14')
+def lesson1_14():
+    return render_template("hel14.html")
+
+
+@app.route('/hel15')
+def lesson1_15():
+    return render_template("hel15.html")
+
+
+@app.route('/lab_hel1')
+def lab_hel1():
+    return render_template("lab_hel1.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
